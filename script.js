474cfe333f108156e720b0c114f7a0a59d955e6d
VartikaAgarwalla <vartikaagarwalla2@gmail.com>
2020-07-30 13:29:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function addItem() {
+    document.getElementById("add-status").innerHTML="Item added to Cart Successfully";
+}
+
+function deleteItem() {
+    document.getElementById("delete-status").innerHTML="Item removed from Cart Successfully";
+}
